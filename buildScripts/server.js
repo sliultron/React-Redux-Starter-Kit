41e069bbc7596ca438ef5a35d6f6ce2dec1d6d58
Li <573094208@MAPLE.fg.rbc.com>
2017-04-10 10:24:11 -0700

    added babel basic configuration

--- buildScripts/server.js
+++ buildScripts/server.js
@@ -1,18 +1,18 @@
-var express = require('express');
-var path = require('path');
-var open = require('open');
+import express from 'express';
+import path from 'path';
+import open from 'open';
 
 
-var port = 3000;
-var app = express();
+const port = 3000;
+const app = express();
 
-app.get('/', function(req, res){
+app.get('/', (req, res)=>{
 
    res.sendfile(path.join(__dirname,'../src/index.html'));
 });
 
 
-app.listen(port, function(err){
+app.listen(port, err =>{
      if(err){
        console.log(err);
      }
